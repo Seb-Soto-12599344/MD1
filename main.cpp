c39b1239ae2477c1883e499c7d8f04a6441e36dd
Sebastian Soto <sebastiansoto@192-168-1-16.tpgi.com.au>
2020-09-21 00:16:30 +1000

    Adding drive, getSteps functions.

--- main.cpp
+++ main.cpp
@@ -1,11 +1,12 @@
 #include <Arduino.h>
 #include <Stepper.h>
 
+
 //Unsure of this value.
 const unsigned int STEPS_PER_REV = 200;
 
 const unsigned int WHEEL_DIAMETER = 95; //mm
-
+const unsigned int WHEEL_CIRCUMFERENCE = 298.45;
 double position_x;
 double position_y;
 
@@ -21,13 +22,68 @@ void loop() {
 
 }
 
-void driveForward(int forwards, int distance) 
+void drive(bool forwards, int distance) 
 {
-  
+  //takes a boolean value for the direction, 1-forward / 0-backwards
+  //takes a distance to be travelled in mm, first convirting to motor steps.
+  int steps = getSteps(distance);
+  switch (forwards)
+  {
+    case 1:
+      while(steps > 0){
+        motorLeft.step(1);
+        motorRight.step(1);
+        steps--;
+      }
+      break;
+    
+    case 0:
+      while(steps > 0){
+        motorLeft.step(-1);
+        motorRight.step(-1);
+        steps--;
+      }
+      break;
+  }
   
 }
 
-int getSteps(int distance)
+void turn(bool clockwise, int angle) 
 {
+  //takes a boolean value for the direction, 1-clockwise / 0-counter-clockwise
+  //takes an angle to be travelled in mm, first converting to motor steps.
+  int steps = getSteps(distance);
+  switch (forwards)
+  {
+    case 1:
+      while(steps > 0){
+        motorLeft.step(1);
+        motorRight.step(1);
+        steps--;
+      }
+      break;
+    
+    case 0:
+      while(steps > 0){
+        motorLeft.step(-1);
+        motorRight.step(-1);
+        steps--;
+      }
+      break;
+  }
+  
+}
 
+int getStepsAngle(int angle)
+{
+  //takes and angle in degrees to be rotated and returns the stepps required to rotate it
+  //rotates the vehicle around the centre of the rear axle.
+}
+
+int getStepsDistance(int distance)
+{
+  //takes a distance in mm and returns the number of motor steps required.
+  double revolutions = distance / WHEEL_CIRCUMFERENCE;
+
+  return STEPS_PER_REV * revolutions;
 }
