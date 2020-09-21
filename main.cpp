f2aed4518884a0a83b449bc1a836b9d2241bf6ef
Sebastian Soto <sebastiansoto@192-168-1-16.tpgi.com.au>
2020-09-21 18:06:09 +1000

    Adding function to rotate pivoting on a wheel.

--- main.cpp
+++ main.cpp
@@ -1,14 +1,17 @@
 #include <Arduino.h>
 #include <Stepper.h>
+#include <math.h>
 
 
 //Unsure of this value.
 const unsigned int STEPS_PER_REV = 200;
 
 const unsigned int WHEEL_DIAMETER = 95; //mm
-const unsigned int WHEEL_CIRCUMFERENCE = 298.45;
-double position_x;
-double position_y;
+const double WHEEL_CIRCUMFERENCE = 298.45; //mm
+const unsigned int CHASSIS_WIDTH = 320; //mm
+const double CHASSIS_CIRCUMFERENCE = CHASSIS_WIDTH * M_PI;
+
+
 
 void setup() {
 
@@ -48,17 +51,19 @@ void drive(bool forwards, int distance)
   
 }
 
-void turn(bool clockwise, int angle) 
+void turnCentre(bool clockwise, int angle) 
 {
   //takes a boolean value for the direction, 1-clockwise / 0-counter-clockwise
   //takes an angle to be travelled in mm, first converting to motor steps.
+
+  //Rotates the vehicle the angle desired, rotating about a point halfway between the wheels.
   int steps = getSteps(distance);
-  switch (forwards)
+  switch (clockwise)
   {
     case 1:
       while(steps > 0){
         motorLeft.step(1);
-        motorRight.step(1);
+        motorRight.step(-1);
         steps--;
       }
       break;
@@ -66,7 +71,7 @@ void turn(bool clockwise, int angle)
     case 0:
       while(steps > 0){
         motorLeft.step(-1);
-        motorRight.step(-1);
+        motorRight.step(1);
         steps--;
       }
       break;
@@ -74,10 +79,71 @@ void turn(bool clockwise, int angle)
   
 }
 
+void turnWheel(bool clockwise, bool wheel, int angle)
+{
+  //takes a boolean value for the direction, 1-clockwise / 0-counter-clockwise
+  //takes a boolean value for the wheel to pivot on, 1-left / 0-right
+  //takes an angle to be travelled in mm, first converting to motor steps.
+
+  //Rotates the vehicle the angle desired, rotating about a point halfway between the wheels.
+  int steps = getSteps(distance);
+  switch (clockwise)
+  {
+    case 1:
+
+      switch(wheel)
+      {
+        case 1:
+          while(steps > 0){
+            motorLeft.step(1);
+            motorRight.step(-10);
+            steps--;
+          }
+          break;
+
+        case 0:
+          while(steps > 0){
+            motorLeft.step(10);
+            motorRight.step(-1);
+            steps--;
+          }
+          break;
+
+      }
+
+    case 0:
+      switch(wheel)
+      {
+        case 1:
+          while(steps > 0){
+            motorLeft.step(-1);
+            motorRight.step(10);
+            steps--;
+          }
+          break;
+
+        case 0:
+          while(steps > 0){
+            motorLeft.step(-10);
+            motorRight.step(1);
+            steps--;
+          }
+          break;
+      }
+      break;
+  }
+}
+
 int getStepsAngle(int angle)
 {
   //takes and angle in degrees to be rotated and returns the stepps required to rotate it
-  //rotates the vehicle around the centre of the rear axle.
+  //rotates the vehicle around the centre of the rear axle. 
+  
+  //gets fraction of a full rotation needing to be turned, multiplied by the circumference of a full rotation.
+  double distance = CHASSIS_CIRCUMFERENCE *  ( angle / 360 );
+
+  return getStepsDistance(distance);
+
 }
 
 int getStepsDistance(int distance)
@@ -85,5 +151,5 @@ int getStepsDistance(int distance)
   //takes a distance in mm and returns the number of motor steps required.
   double revolutions = distance / WHEEL_CIRCUMFERENCE;
 
-  return STEPS_PER_REV * revolutions;
+  return (int)(STEPS_PER_REV * revolutions);
 }
